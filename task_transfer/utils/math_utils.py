4ac9ea07d28ba3bf1e8a3cdd2a0c665b1eee3598
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-05-07 14:13:08 +0200

    Add custom von mises implementation

--- task_transfer/utils/math_utils.py
+++ task_transfer/utils/math_utils.py
@@ -1,5 +1,14 @@
 import torch
+from scipy.special import i0
 
 
 def is_pos_def(x):
     return torch.all(torch.linalg.eigvals(x).real > 0)
+
+
+def cos2_von_mises(x, loc, concentration, normalized=True):
+    exponent = torch.exp(concentration * torch.cos(2 * (x - loc)))
+    if normalized:
+        return exponent / (2 * torch.pi * i0(concentration))
+    else:
+        return exponent
