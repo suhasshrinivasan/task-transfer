2a74c5dc0706faa25ceeffd0a8962dc33af57808
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-05-15 20:28:49 +0200

    Add likelihood config

--- experiments/orientation_discrimination/learning/generative_model_configs.py
+++ experiments/orientation_discrimination/learning/generative_model_configs.py
@@ -1,10 +1,17 @@
-import numpy as np
-import torch
-
-marginal_flow_model_config = {
+marginal_flow_prior = {
     "dims": None,  # to be determined from data
     "flow_depth": 2,
     "flow_initial_nonlinearity": "inv_softplus",  # flow direction is data -> latent
     "flow_nonlinearity": "tanh",
     "flow_base_distribution": "normal",
+}
+
+gaussian_linear_likelihood = {
+    "in_features": None,  # to be determined from data
+    "out_features": None,  # to be determined from data
+    "n_layers": 1,
+    "nonlin": "none",
+    "dropout_rate": 0.0,
+    "init_std": 1e-3,
+    "pre_clamp_scale": True,
 }
