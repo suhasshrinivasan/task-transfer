5f7cb46821ad4bdf11d63f808d49857eb9ab24b0
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-05-15 20:29:35 +0200

    Add trainer config

---
+++ experiments/orientation_discrimination/learning/training_configs.py
@@ -0,0 +1,11 @@
+generative_model_trainer_config = {
+    "lr": 1e-3,
+    "weight_decay": 0,
+    "n_epochs": 100,
+    "batch_size": 128,
+    "early_stopping_threshold": 10,
+    "early_stopping_patience": 10,
+    "logging_type": "stdout",
+    "train_prop": 0.6,
+    "val_prop": 0.2,
+}
