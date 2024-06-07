fe57207e49b64446e78556e6457d329cf89ee4b7
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-06-07 11:00:13 +0200

    Add cond nll

--- task_transfer/ml_lib/loss_criteria.py
+++ task_transfer/ml_lib/loss_criteria.py
@@ -10,7 +10,7 @@ def joint_nll(model, batch):
         torch.Tensor: The negative log-likelihood of the batch of data.
     """
     response, obs = batch
-    return -model.log_prob(response, obs)
+    return -model(response, obs)
 
 
 def marginal_nll(model, batch, data_dim):
@@ -26,4 +26,22 @@ def marginal_nll(model, batch, data_dim):
         torch.Tensor: The negative log-likelihood of the batch of data.
     """
     data = batch[data_dim]
-    return -model.log_prob(data)
+    return -model(data)
+
+
+def conditional_nll(model, batch, data_dim, cond_dim):
+    """
+    Computes the negative log-likelihood of the batch of data using the conditional model.
+
+    Args:
+        model (gensn.distributions): The conditional model
+        batch (torch.Tensor): The batch of data of shape (batch_size, data_dim).
+        data_dim (int): The dimensionality of the data.
+        cond_dim (int): The dimensionality of the conditioning variable.
+
+    Returns:
+        torch.Tensor: The negative log-likelihood of the batch of data.
+    """
+    data = batch[data_dim]
+    cond = batch[cond_dim]
+    return -model(data, cond=cond)
