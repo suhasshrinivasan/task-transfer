5348ab994f7f1ab7f49513b8d59d8854c38e7ab9
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-07-10 05:07:35 -0700

    re

--- task_transfer/ml_lib/model_building.py
+++ task_transfer/ml_lib/model_building.py
@@ -39,23 +39,29 @@ def build_transform_sequence(
     # stack sequence of affine + nonlin
     # NOTE: this assumes that sharing identical nonlin layer
     # multiple times in a network is okay. Probably fine.
-    nonlin_layer = inv_nonlins[nonlin]()
+    if nonlin != "none":
+        nonlin_layer = inv_nonlins[nonlin]()
+
     if affine_type == "factorized":
         affine_layer_class = IndependentAffine
     elif affine_type == "full":
         affine_layer_class = Affine
     elif affine_type == "lowrank":
         return NotImplementedError("Lowrank not implemented")
+    elif affine_type == "none":
+        pass
     else:
         raise ValueError("Unknown affine type")
 
     for _ in range(depth):
-        affine_layer = affine_layer_class(dims)
-        initialize_affine_transform_(
-            affine_layer, init_mean=affine_init_mean, init_std=affine_init_std
-        )
-        layers.append(affine_layer)
-        layers.append(nonlin_layer)
+        if affine_type != "none":
+            affine_layer = affine_layer_class(dims)
+            initialize_affine_transform_(
+                affine_layer, init_mean=affine_init_mean, init_std=affine_init_std
+            )
+            layers.append(affine_layer)
+        if nonlin != "none":
+            layers.append(nonlin_layer)
 
     # why is this separated?
     if depth > 0:
@@ -81,24 +87,25 @@ def build_flow_model(
     flow_final_nonlinearity="none",
     affine_type="factorized",
 ):
+    # for normal base distributions
+    if "_lrmn" in flow_base_distribution:  # _lrmn denotes "learn mean"
+        loc = torch.nn.Parameter(torch.zeros(dims))
+    else:
+        loc = torch.zeros(dims)
+
+    flow_base_distribution = flow_base_distribution.replace("_lrmn", "")
     if flow_base_distribution == "normal":
-        flow_base_distribution = G.IndependentNormal(
-            loc=torch.zeros(dims), scale=torch.ones(dims)
-        )
+        flow_base_distribution = G.IndependentNormal(loc=loc, scale=torch.ones(dims))
     elif flow_base_distribution == "uniform":
         flow_base_distribution = TrainableDistributionAdapter(
             G.wrap_with_indep(D.Uniform), low=torch.zeros(dims), high=torch.ones(dims)
         )
     elif flow_base_distribution == "multivariate_normal":
-        # loc = torch.nn.Parameter(torch.randn(dims))
-        loc = torch.zeros(dims)
         cov = gensn.parameters.Covariance(n_dims=dims)
         flow_base_distribution = G.TrainableDistributionAdapter(
             D.MultivariateNormal, loc=loc, covariance_matrix=cov
         )
     elif "lowrank_multivariate_normal" in flow_base_distribution:
-        # loc = torch.nn.Parameter(torch.randn(dims))
-        loc = torch.zeros(dims)
         rank = int(flow_base_distribution.split("_")[-1])
         cov = gensn.parameters.Covariance(n_dims=dims, rank=rank)
         flow_base_distribution = G.TrainableDistributionAdapter(
