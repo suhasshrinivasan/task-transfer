70e70865a250ded75b475cb7832895ecdcc6479a
Suhas Shrinivasan <suhasshrinvasan@gmail.com>
2024-07-19 10:20:50 +0200

    Add multiple prior visualization

--- task_transfer/evaluation/evaluate_generative_model.py
+++ task_transfer/evaluation/evaluate_generative_model.py
@@ -1097,3 +1097,83 @@ def evaluate_generative_model(
         **eval_params["conditional_params"],
     )
     return
+
+
+def visualize_marginal_flow(
+    models,
+    data_loader,
+    device="cpu",
+    density_support=(1e-3, 10),
+    density_n_samples=1000,
+    dims_to_plot=range(45),
+    fig_dpi=300,
+    linewidth=3,
+    fontsize=10,
+    plot_xlim=(0, 7),
+    plot_ylim=(0, 1),
+    data_color="darkorange",
+    data_alpha=0.4,
+    fig_save_dir=Path("/src/project/figures/learning/"),
+    **catch_all,
+):
+    all_responses = []
+    for batch in data_loader:
+        responses, _ = batch
+        all_responses.append(responses.to(device))
+    all_responses = torch.cat(all_responses, dim=0)
+    n_dims_to_plot = len(dims_to_plot)
+    with torch.no_grad():
+        n_dims_all = all_responses.shape[1]
+        x = (
+            torch.linspace(density_support[0], density_support[1], density_n_samples)
+            .repeat(n_dims_all, 1)
+            .T
+        )
+        fig, axs = plt.subplots(
+            7,
+            7,
+            sharey=True,
+            dpi=fig_dpi,
+        )
+        for idx, ax in zip(dims_to_plot, axs.ravel()):
+            sns.histplot(
+                all_responses[:, idx].detach().cpu(),
+                ax=ax,
+                stat="density",
+                element="step",
+                color=data_color,
+                alpha=data_alpha,
+                label="Data",
+            )
+        colors = sns.color_palette("tab10", n_colors=len(models))
+        for model, color, label, linestyle in zip(
+            models, colors, catch_all["labels"], catch_all["linestyles"]
+        ):
+            flow_density = model.factorized_log_prob(x.to(device)).exp().cpu().numpy()
+            print(flow_density.shape)
+            for idx, ax in zip(dims_to_plot, axs.ravel()):
+                ax.plot(
+                    x[:, idx].detach().cpu(),
+                    flow_density[:, idx],
+                    linewidth=linewidth,
+                    color=color,
+                    label=label,
+                    linestyle=linestyle,
+                )
+                ax.set_xlim(*plot_xlim)
+                ax.set_ylim(*plot_ylim)
+                ax.axis("off")
+            # ax.tick_params(axis="both", which="both", labelsize=fontsize)
+            # ax.set_ylabel("$p(x)$", fontsize=fontsize)
+            # ax.set_xlabel("x", fontsize=fontsize)
+            for ax in axs.ravel()[n_dims_to_plot:]:
+                ax.axis("off")
+        handles, labels = axs.flatten()[0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc="lower right", fontsize=fontsize)
+        fig.savefig(
+            fig_save_dir / catch_all["fig_name"],
+            bbox_inches="tight",
+            transparent=True,
+        )
+        # close the figure to avoid memory leak
+        plt.close(fig)
